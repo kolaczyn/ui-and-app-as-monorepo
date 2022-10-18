1fe937c79212cd81e79b293c3eef0228901185d3
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-10-18 21:40:35 +0200

    make utils package

---
+++ yarn-workspaces/packages/utils/getRandomNumber.js
@@ -0,0 +1,6 @@
+/** always return 6, determined by a random roll of the dice */
+const getRandomNumber = () => 6;
+
+module.exports = {
+  getRandomNumber,
+};
---
+++ yarn-workspaces/packages/utils/index.js
@@ -0,0 +1,9 @@
+const add = require("./math/add");
+const subtract = require("./math/subtract");
+const getRandomNumber = require("./getRandomNumber");
+
+module.exports = {
+  add,
+  subtract,
+  getRandomNumber,
+};
