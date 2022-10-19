c2e0380bae2bff878ba0c64d89e9807015917d52
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-10-19 03:56:49 +0200

    add emotion

---
+++ packages/emotion/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@yarn-workspaces/styled-components",
+      formats: ["es", "umd", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "react-dom",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
